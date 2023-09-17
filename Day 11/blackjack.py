b147ebd887634192bf1fb4a8758d280411f8b3ca
Prabhat Kumar <prabhat.kumar.hisar@gmail.com>
2023-09-17 22:07:38 +0530

    Change names of a variable is_game_over to game_over and function from play_game to start_game.

--- Day 11/blackjack.py
+++ Day 11/blackjack.py
@@ -4,7 +4,8 @@ import os
 
 def clear():
   os.system('cls')
-def deal_card():
+  
+def card():
   """Returns a random card from the deck."""
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
   card = random.choice(cards)
@@ -41,36 +42,36 @@ def compare(user_score, computer_score):
   else:
     return "You lose 😤"
 
-def play_game():
+def start_game():
 
   print(logo)
 
   user_cards = []
   computer_cards = []
-  is_game_over = False
+  game_over = False
 
   for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
+    user_cards.append(card())
+    computer_cards.append(card())
 
 
-  while not is_game_over:
+  while not game_over:
     user_score = calculate_score(user_cards)
     computer_score = calculate_score(computer_cards)
     print(f"   Your cards: {user_cards}, current score: {user_score}")
     print(f"   Computer's first card: {computer_cards[0]}")
 
     if user_score == 0 or computer_score == 0 or user_score > 21:
-      is_game_over = True
+      game_over = True
     else:
       user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
       if user_should_deal == "y":
-        user_cards.append(deal_card())
+        user_cards.append(card())
       else:
-        is_game_over = True
+        game_over = True
 
   while computer_score != 0 and computer_score < 17:
-    computer_cards.append(deal_card())
+    computer_cards.append(card())
     computer_score = calculate_score(computer_cards)
 
   print(f"   Your final hand: {user_cards}, final score: {user_score}")
@@ -79,4 +80,4 @@ def play_game():
 
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
   clear()
-  play_game()
+  start_game()
