f026189d7ab04d73576fd13172c6daf6c436f1c9
Prabhat Kumar <prabhat.kumar.hisar@gmail.com>
2023-11-03 18:52:11 +0530

    Modified main file in Day 25 folder with exception handling.

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,11 +1,19 @@
 import pandas
 df=pandas.read_csv("Day 25/nato_phonetic_alphabet.csv")
 
-#TODO 1. Create a dictionary in this format:
+
 student_dict={row.letter:row.code for (index,row) in df.iterrows()}
-user_input=input("Enter a word: ")
-user_input=user_input.upper()
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_list=[student_dict[i] for i in user_input if i in student_dict]
-print(user_list)
+
+def phonetic(): 
+    user_input=input("Enter a word: ").upper()
+    try:
+        user_list=[student_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please!")
+        phonetic()
+    else:
+        print(user_list)
+
+phonetic()
+
 
